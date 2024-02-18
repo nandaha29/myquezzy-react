77c410127e4b38b8408dfbda6a5916543662e212
Nanda hafiza <yusronnandahafiza@gmail.com>
2024-02-18 22:48:09 +0700

    update navbar

--- myquiz-app/src/components/Fragment/Navbar/Navbar.jsx
+++ myquiz-app/src/components/Fragment/Navbar/Navbar.jsx
@@ -22,12 +22,6 @@ function Navbar() {
                   <a href="/" className=" text-gray-500 hover:text-violet-500 rounded-md px-2 py-2 text-sm font-medium" aria-current="page">
                     Dashboard
                   </a>
-                  <a href="/login" className=" text-gray-500 hover:text-violet-500 rounded-md px-2 py-2 text-sm font-medium" aria-current="page">
-                    Login
-                  </a>
-                  <a href="/register" className=" text-gray-500 hover:text-violet-500 rounded-md px-2 py-2 text-sm font-medium" aria-current="page">
-                    Regist
-                  </a>
                 </div>
               </div>
             </div>
